4f1bd16c79588d57fa4f4088cbe75171f08f7337
Souad <220113@fppiramide.com>
2024-02-19 14:18:50 +0100

    up

--- js/accesibility.js
+++ js/accesibility.js
@@ -1,12 +1,32 @@
-document.getElementById('pausar').addEventListener('click', pausarAnimaciones);
+
+document.getElementById("pausar").addEventListener('click', pausarAnimaciones);
+
 function pausarAnimaciones(){
-    const barraScroll = document.getElementById('barra-scroll');
+    const barraScroll = document.getElementById("barra-scroll");
     const llama = document.getElementById('llama');
     const star = document.getElementById('star');
-    const luna = document.getElementById('luna')
+    const luna = document.getElementById('luna');
+   
     if(barraScroll.style.animationPlayState === 'running'){
         barraScroll.style.animationPlayState = 'paused';
     }else{
         barraScroll.style.animationPlayState = 'running';
     }
+
+    if(llama.style.animationPlayState === 'running'){
+        llama.style.animationPlayState = 'paused';
+    }else{
+        llama.style.animationPlayState = 'running';
+    }
+
+    if(star.style.animationPlayState === 'running' && luna.style.animationPlayState === 'running'){
+        star.style.animationPlayState = 'paused';
+        luna.style.animationPlayState === 'running'
+    }else{
+        star.style.animationPlayState = 'running';
+        luna.style.animationPlayState === 'running'
+    }
+
+
 }
+
